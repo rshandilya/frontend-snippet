e7ceb30ffc399c2de1d5b3d633fc98e3b035c28c
rshandilya <prof.rshandilya@gmail.com>
2018-09-02 06:09:36 +0530

    Update ajax.js

--- JS/ajax.js
+++ JS/ajax.js
@@ -17,12 +17,33 @@ httpRequest.open('GET', 'http://www.example.org/some.file', true);
 httpRequest.send();
 
 
+// STEP2 Handling Response
 
+/*****  httprequest.readystate states
+    0 (uninitialized) or (request not initialized)          - UNSENT  
+    1 (loading) or (server connection established)           -OPEN
+    2 (loaded) or (request received)                         -HEADERS_RECEIVED
+    3 (interactive) or (processing request)                   LOADING
+    4 (complete) or (request finished and response is ready) -DONE
+*/
+// ===DONE (state 4)
+if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    // Everything is good, the response was received.
+} else {
+    // Not ready yet.
+}
 
+// check the HTTP response status codes 
 
+if (httpRequest.status === 200) {
+    // Perfect!
+} else {
+    // There was a problem with the request.
+    // For example, the response may have a 404 (Not Found)
+    // or 500 (Internal Server Error) response code.
+}
 
-
-
-
-
+// Two options to access data
+httpRequest.responseText // – returns the server response as a string of text
+httpRequest.responseXML 
 
